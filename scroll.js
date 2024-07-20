f91d409238671f5278b0498267c91bafe4cb5ef2
Samant Patil <samantpatil.0425@gmail.com>
2024-07-20 23:43:05 +0530

    Delete scroll.js

--- scroll.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('scroll', function() {
-    const backgroundImage = document.querySelector('.background-image');
-    if (window.scrollY > window.innerHeight / 2) {
-        document.body.classList.add('scrolled');
-    } else {
-        document.body.classList.remove('scrolled');
-    }
-});
